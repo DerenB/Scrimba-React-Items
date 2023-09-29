a5d690197d2652f8356cdf28ee46fc0b941f8155
DerenB <dbozerprogramming@gmail.com>
2023-09-28 23:53:47 -0400

    React Notes

--- AA-NetNinjaPlaylist/dojo-blog/src/App.js
+++ AA-NetNinjaPlaylist/dojo-blog/src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import Navbar from "./Navbar";
 import Home from "./Home";
 
@@ -18,6 +17,7 @@ function App() {
     <div className="App">
       <Navbar />
       <div className="content">
+        <Home />
         <h1>{ title }</h1>
         <p>Liked { likes } times</p>
         <p>{person.name} is {person.age}</p>
