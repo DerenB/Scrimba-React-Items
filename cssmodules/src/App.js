ae71799c20c54bad04e64b1c1e1fcc7986e7513e
DerenB <dbozerprogramming@gmail.com>
2023-09-29 00:23:09 -0400

    CSS Module Demo

--- cssmodules/src/App.js
+++ cssmodules/src/App.js
@@ -3,9 +3,16 @@ import { GreenBox } from './GreenBox';
 import { RedBox } from './RedBox';
 
 function App() {
+
+  const sourceLink = "https://dev.to/eransakal/how-to-isolate-component-styles-in-react-using-css-modules-mkm";
+
   return (
     <div className="App">
       <p>Boxes with CSS Module</p>
+      <div>
+        <a href={sourceLink}>Source Article</a>
+      </div>
+      <br />
       <GreenBox />
       <RedBox />
     </div>
